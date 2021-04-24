6e4bf71b523d38eb9ddf4bae5b2ee0d80506190d
unknown <shashanktaneja72@gmail.com>
2021-04-25 04:59:54 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require(__dirname + "/config/db.js");
+const path = require("path");
 
 const app = express();
 
@@ -9,15 +10,21 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/",function(req,res){
-    res.send("API running");
-});
-
 //define routes
 app.use("/api/users", require(__dirname + "/router/api/users.js"));
 app.use("/api/auth", require(__dirname + "/router/api/auth.js"));
 app.use("/api/posts", require(__dirname + "/router/api/posts.js"));
 app.use("/api/profile", require(__dirname + "/router/api/profile.js"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 const port = process.env.PORT || 5000;
